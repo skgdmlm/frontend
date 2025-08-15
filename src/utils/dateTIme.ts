83e763935d88fb7b02162b14100172adc2cf0376
SKGD <skgdmlm@gmail.com>
2025-08-15 20:17:06 +0530

    another approch to fix timezone issue

--- src/utils/dateTIme.ts
+++ src/utils/dateTIme.ts
@@ -1,13 +1,17 @@
 import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
-
+import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-dayjs.tz.setDefault('Asia/Kolkata');
-
 export const tableFormat = (date: string | Date) => {
-    return dayjs(date).format('YYYY-MM-DD');
+  // Explicitly parse the date as UTC first
+  const utcDate = dayjs.utc(date);
+  
+  // Then convert to India timezone
+  const indiaDate = utcDate.tz('Asia/Kolkata');
+  
+  // Format the date
+  return indiaDate.format('YYYY-MM-DD');
 };
